a5a03348cfaa14d6dc5b30f65c81151c82b2f8f9
Giro1230 <hellowin1230@gmail.com>
2021-05-22 01:38:23 +0900

    test

---
+++ Source/Helloword2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int plus(int x, int y) {
+	int z;
+	z = x + y;
+	println(z);
+	return z;
+};
+
+
+int main() {
+	int result;
+	printf "Hello World";
+	result = plus(1, 2);
+	println(result);
+
+	return 0;
+}
+
